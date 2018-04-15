d0b360296b2012c3be3f25a45913dd2cbae61fad
alibeibei <1939612669@qq.com>
2018-04-15 15:17:30 +0800

    提交

---
+++ enreka/src/main/java/cn/alibeibei/enreka/EnrekaApplication.java
@@ -0,0 +1,12 @@
+package cn.alibeibei.enreka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnrekaApplication.class, args);
+	}
+}
